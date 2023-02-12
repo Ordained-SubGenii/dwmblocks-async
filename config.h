15ce115a95f110dafbaf01476384224ace4cd34a
feindsdeluna <jedisocialist@protonmail.com>
2023-02-12 22:57:58 +0100

    added sd-df

--- config.h
+++ config.h
@@ -4,7 +4,8 @@
 
 const Block blocks[] = {
 	BLOCK("sb-wifi",    30,    0),
-	BLOCK("sb-volume",   0,    5),
+	BLOCK("sb-df",      420,   0),
+  BLOCK("sb-volume",   0,    5),
 	BLOCK("sb-battery", 60,    0),
 	BLOCK("sb-date",    60,    0),
 };
